7dea68518adc69d7d2d771fbc7a9c1738b40faff
Ahmad Hasan Ali <87076122+Ahmadhasanali@users.noreply.github.com>
2024-01-19 10:09:33 +0700

    Add: todo-comments

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,17 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  --
+  
+  -- NOTE: Package for todo comments
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = { "User Astrofile" },
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODO's on TeleScope" },
+    },
+  },
 }
